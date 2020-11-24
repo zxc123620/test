87f64446f43fea45ce06e3409f005cc5937c74f5
zxc123620 <74952659+zxc123620@users.noreply.github.com>
2020-11-24 17:00:33 +0800

    第一次修改文件

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld{
   public static void main(String[] args){
-    System.out.println("hello world");
+    System.out.println("hello world!");
+    System.out.println("hello java");
   }
 }
