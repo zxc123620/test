58eef9914155e5e7dbee74abd1ae9ddb0db6e15f
zxc123620 <74952659+zxc123620@users.noreply.github.com>
2020-11-25 17:18:31 +0800

    Delete ButtonReseda.java

--- ButtonReseda.java
+++
@@ -1,8 +0,0 @@
-package Demo03AbstractFactory.ButtonPac;
-
-public class ButtonReseda implements ButtonImpl{
-    @Override
-    public void showButton() {
-        System.out.println("展示浅绿色按钮");
-    }
-}
