355d18c323547d7d27949005a60a926cd19dacbd
zxc123620 <74952659+zxc123620@users.noreply.github.com>
2020-11-25 17:17:58 +0800

    Delete ButtonBlue.java

--- ButtonBlue.java
+++
@@ -1,8 +0,0 @@
-package Demo03AbstractFactory.ButtonPac;
-
-public class ButtonBlue implements ButtonImpl{
-    @Override
-    public void showButton() {
-        System.out.println("展示蓝色按钮");
-    }
-}
